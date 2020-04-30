25352595e864b3eb1b1f07ff9c14e7a2ab7b88fe
Ivan <lysko1999i@gmail.com>
2020-04-30 13:02:41 +0300

    fixed current weather

--- src/redux/forecast/forecastOperations.ts
+++ src/redux/forecast/forecastOperations.ts
@@ -2,6 +2,7 @@ import * as actions from './forecastActions';
 import { getWeatherForecast } from '../../services/weatherAPI';
 
 import { Dispatch } from '../store';
+import currentForecastFilter from '../../utils/helpers';
 import { Error, CityWeather } from './forecastTypes';
 
 interface AxiosDataType {
@@ -20,8 +21,12 @@ const getWeather = (query: string) => (dispatch: Dispatch): void => {
 
   getWeatherForecast(query)
     .then((res: AxiosType) => {
-      dispatch(actions.currentWeatherSuccess(res.data.list));
-      dispatch(actions.weekWeatherSuccess(res.data.list));
+      const { list } = res.data;
+
+      const currentForecast = list.filter((listItem): boolean => currentForecastFilter(listItem));
+
+      dispatch(actions.currentWeatherSuccess(currentForecast));
+      dispatch(actions.weekWeatherSuccess(list));
     })
     .catch((err: Error) => {
       dispatch(actions.currentWeatherError(err));
